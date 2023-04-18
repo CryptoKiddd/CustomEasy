2b97d5671b63cd2934555918b9f0adb2b4b26f26
armaz avaliani <avaliani1232@gmail.com>
2023-04-19 03:25:22 +0400

    shirt model

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-const Canvas = () => {
+import {Canvas} from '@react-three/fiber'
+import Shirt from './Shirt'
+import Backdrop from './Backdrop'
+import CameraRig from './CameraRig'
+import {Environment, Center} from '@react-three/drei'
+const CanvasModel = () => {
   return (
-    <div>index</div>
+    <Canvas className='w-full max-w-full h-full transition-full ease-in' shadows camera={{position:[0,0,0], fov:25}} gl={{preserveDrawingBuffer:true}}>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+      <CameraRig>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
---
+++ src/components/Tab.jsx
@@ -0,0 +1,9 @@
+import {useSnapshoot} from 'valtio'
+import state from '../store'
+const Tab = ({tab,isActiveTab,isFilterTab,handleClick}) => {
+  return (
+    <div>Tab</div>
+  )
+}
+
+export default Tab
